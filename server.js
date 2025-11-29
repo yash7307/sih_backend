a1d15981ca60a6b4094c53389e098f5883781eb6
Yash Sahu <b523076@iiit-bh.ac.in>
2025-11-29 06:52:03 +0530

    Increase payload limit to 50mb

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const User = require("./models/User");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // 📌 Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URL)
